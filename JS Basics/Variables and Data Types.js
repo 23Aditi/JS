94f18a44fba6f0678703154a67de45a7c2cd96d5
ADITI <aditi23github@gmail.com>
2024-12-19 15:37:59 +0000

    commit

---
+++ JS Basics/Variables and Data Types.js
@@ -0,0 +1,44 @@
+var name = "Aditi";
+var age = 21;
+const pi = 3.14;
+console.log(name);
+console.log(age);
+console.log(pi);
+age = 56;
+console.log(age);
+//pi = 45;
+//console.log(pi);
+let isStudent = true;
+console.log(isStudent);
+let isLoggedIn = false;
+console.log(isLoggedIn);
+let city;
+console.log(city);
+let temp = null;
+console.log(temp);
+
+let p = {
+    name : "Aditi",
+    age : 18,
+    city : "Pune"
+};
+
+
+/*
+
+/workspaces/JS/JS Basics/Variables and Data Types.js:9
+pi = 45;
+   ^
+
+TypeError: Assignment to constant variable.
+    at Object.<anonymous> (/workspaces/JS/JS Basics/Variables and Data Types.js:9:4)
+    at Module._compile (node:internal/modules/cjs/loader:1469:14)
+    at Module._extensions..js (node:internal/modules/cjs/loader:1548:10)
+    at Module.load (node:internal/modules/cjs/loader:1288:32)
+    at Module._load (node:internal/modules/cjs/loader:1104:12)
+    at Function.executeUserEntryPoint [as runMain] (node:internal/modules/run_main:173:12)
+    at node:internal/main/run_main_module:28:49
+
+Node.js v20.18.1
+@23Aditi ➜ /workspaces/JS/JS Basics (main) $ 
+*/
