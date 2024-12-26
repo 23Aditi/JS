306922f3a0e38b1c5c86fbe83a03aa930c2ebd2d
ADITI <aditi23github@gmail.com>
2024-12-26 08:47:27 +0000

    js

--- JS Basics/Control Flow/ControlFlow.js
+++ JS Basics/Control Flow/ControlFlow.js
@@ -1,6 +1,28 @@
+// < , > , <= , >= , == , != , ===
+const isLoggedIn = true;
+if(2=="2"){
+    console.log("executed");    //executed
+}
+
+if(2==="2"){
+    console.log("executed");    //doesn't go inside loop
+}else{
+    console.log("not executed");    //not executed
+}
 
 
+const score = 200
+if(score > 100){
+    const power = "fly";
+    console.log(`User power : ${power}`);   //User power : fly
+}
+//console.log(`User power : ${power}`);   //ReferenceError: power is not defined
 
+//shorthand
+const balance = 100;
+if(balance > 500) console.log("test");
+else if(balance > 750 && balance >0) console.log("test1")
+else console.log("test2");  //test2
 
-
+//  || , && , ===
 
