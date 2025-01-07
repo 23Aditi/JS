35fafbcd4793b14e092b7394e860b24b76ecdfac
ADITI <aditi23github@gmail.com>
2025-01-07 13:36:23 +0000

    the magic begins !!!

---
+++ JS Basics/Projects/Digital Clock/script.js
@@ -0,0 +1,11 @@
+const clock = document.getElementById('clock');
+//console.log(clock);
+let Date = new Date()
+console.log(date.toLocalTimeString());
+
+setInterval(function(){},1000) // time in milli seconds
+
+
+
+
+
